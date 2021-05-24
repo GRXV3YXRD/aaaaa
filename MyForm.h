223920f56eca650a5e2386d38f5d5a45db3316f4
Слава Фролова <aaaaa_000v@hotmail.com>
2021-05-24 16:02:27 +0600

    First commit

---
+++ MyForm.h
@@ -0,0 +1,245 @@
+#pragma once
+
+namespace Project {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::Data::OleDb;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+
+
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label5;
+
+
+	protected:
+
+
+
+
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(235, 244);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(157, 42);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Зарегистрироваться";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click_1);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(292, 79);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(157, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(292, 131);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(157, 20);
+			this->textBox2->TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(292, 186);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(157, 20);
+			this->textBox3->TabIndex = 4;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(256, 25);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(113, 19);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Регистрация";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(231, 80);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(55, 19);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Логин";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(221, 129);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(65, 19);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Пароль";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(143, 184);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(143, 19);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Номер телефона";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 268);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(91, 30);
+			this->button2->TabIndex = 9;
+			this->button2->Text = L"Закрыть";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click_2);
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(455, 82);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(0, 13);
+			this->label5->TabIndex = 10;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(661, 310);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+	private: System::Void button1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+		label5->Text = L"";
+
+		String^ connectionString = "provider = Microsoft.Jet.OLEDB.4.0; data source = Database1.mdb";
+		OleDbConnection^ conn = gcnew OleDbConnection(connectionString);
+		OleDbCommand^ command = conn->CreateCommand();
+
+		if (textBox1->Text == "") {
+			label5->Text = L"Нельзя оставить поле пустым";
+		}
+		else {
+			conn->Open();
+			command->CommandText = ("INSERT INTO table1 (log, pass, phone) VALUES (" + this->textBox1->Text + " , " + textBox2->Text + " , " + textBox3->Text + ")");
+			command->Parameters->AddWithValue("INSERT INTO table1 (log) VALUES", textBox1->Text);
+			command->Parameters->AddWithValue("INSERT INTO table1 (pass) VALUES", textBox2->Text);
+			command->Parameters->AddWithValue("INSERT INTO table1 (phone) VALUES", textBox3->Text);
+			command->ExecuteNonQuery();
+			conn->Close();
+		}
+
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void textBox3_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((e->KeyChar < 48 || e->KeyChar > 57) && e->KeyChar != 8) {
+		e->Handled = true;
+		return;
+	}
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button2_Click_2(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
+};
+}
